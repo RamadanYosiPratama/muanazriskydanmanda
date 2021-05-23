2c6642a9674cf24b4b3a95caf5b07c8673bb54b4
RamadanYosiPratama <ramadanyosi30@gmail.com>
2021-05-24 03:03:46 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Arrived from './components/Arrived';
+import Asside from './components/Asside';
+import Browse from './components/Browse';
+import Clients from './components/Clients';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Hero from './components/Hero';
+
+
+function App() {
+  return (
+    <>
+    <Header/>
+    <Hero/>
+    <Browse/>
+    <Arrived/>
+    <Footer/>
+    </>
+    
+  );
+}
+
+export default App;
---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+function Footer() {
+    return (
+        <footer className="flex text-center px-4 py-8 justify-center">
+        <p className="text-sm">
+          Copyright 2021 • All Rights Reserved Anniversary by Muanas Rizky Mubarok
+        </p>
+      </footer>
+    )
+}
+
+export default Footer;
